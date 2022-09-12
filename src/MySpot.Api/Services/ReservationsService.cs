532bd3d44cc54d23a492c9cd04edd055b8b4a3fd
bpelikan <bartoszpelikan@gmail.com>
2022-09-12 21:54:27 +0200

    3.1 Pierwsza abstrakcja

--- src/MySpot.Api/Services/ReservationsService.cs
+++ src/MySpot.Api/Services/ReservationsService.cs
@@ -6,13 +6,14 @@ namespace MySpot.Api.Services
 {
     public sealed class ReservationsService
     {
+        private static readonly Clock Clock = new();
         private static readonly List<WeeklyParkingSpot> WeeklyParkingSpots = new()
         {
-            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000001"), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P1"),
-            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000002"), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P2"),
-            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000003"), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P3"),
-            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000004"), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P4"),
-            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000005"), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P5"),
+            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000001"), Clock.Current(), Clock.Current().AddDays(7), "P1"),
+            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000002"), Clock.Current(), Clock.Current().AddDays(7), "P2"),
+            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000003"), Clock.Current(), Clock.Current().AddDays(7), "P3"),
+            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000004"), Clock.Current(), Clock.Current().AddDays(7), "P4"),
+            new WeeklyParkingSpot(Guid.Parse("00000000-0000-0000-0000-000000000005"), Clock.Current(), Clock.Current().AddDays(7), "P5"),
         };
 
         public ReservationDto Get(Guid id)
@@ -36,7 +37,7 @@ namespace MySpot.Api.Services
 
             var reservation = new Reservation(command.ReservationId, command.ParkingSpotId, command.EmployeeName, 
                 command.LicensePlate, command.Date);
-            weeklyParkingSpot.AddReservation(reservation);
+            weeklyParkingSpot.AddReservation(reservation, Clock.Current());
 
             return reservation.Id;
         }
@@ -51,7 +52,7 @@ namespace MySpot.Api.Services
             if(existingReservation is null)
                 return false;
 
-            if(existingReservation.Date <= DateTime.UtcNow.Date)
+            if(existingReservation.Date <= Clock.Current().Date)
                 return false;
 
             existingReservation.ChangeLicencePlate(command.LicencePlate);
